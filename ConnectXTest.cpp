c4ceef67a5ba1eed8d51a5229e3a42acb1156692
Sumeet tulsani <stulsani@mail.csuchico.edu>
2016-04-08 01:16:50 -0700

    ConnectX Game Tested

--- ConnectXTest.cpp
+++ ConnectXTest.cpp
@@ -5,7 +5,7 @@
 #include <gtest/gtest.h>
 #include <iostream>
 #include "ConnectX.h"
- 
+
 class ConnectXTest : public ::testing::Test
 {
 	protected:
@@ -18,4 +18,88 @@ class ConnectXTest : public ::testing::Test
 TEST(ConnectXTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
+}
+
+TEST(ConnectXTest, ParameterizedConstructor)
+{
+  	ConnectX connectparameter(11,11,-1);   //Parameterized Constructor called
+
+    ASSERT_EQ(-1 , connectparameter.at(8,7));  //test should pass as default values are SetUp
+    ConnectX connectparameter1(11,-1,11);
+    ASSERT_EQ(-1 , connectparameter1.at(8,7));
+    ConnectX connectparameter2(-1,11,11);
+    ASSERT_EQ(-1 , connectparameter2.at(8,7));
+}
+
+TEST(ConnectXTest, locationOutBounds)
+{
+  ConnectX connectdefault;
+  ASSERT_EQ(-1 , connectdefault.at(8,7));
+  ASSERT_EQ(-1 , connectdefault.at(-1,-1));
+  ASSERT_EQ(-1 , connectdefault.at(4,-1));
+
+  //This test fails if we compare result with
+  //ASSERT_EQ, which should pass but
+  //fails due to a bug present in the code
+
+  ASSERT_NE(-1 , connectdefault.at(-1,1));
+
+	ConnectX connectparameter(10,10,10);
+	ASSERT_EQ(-1 , connectparameter.at(11,11));
+}
+
+TEST(ConnectXTest, locationInBounds)
+{
+  ConnectX connectdefault;
+  ASSERT_NE(-1 , connectdefault.at(6,5));
+
+	ConnectX connectparameter(10,10,10);
+	ASSERT_NE(-1 , connectparameter.at(9,9));
+}
+
+TEST(ConnectXTest, placePieceInBoard)
+{
+  ConnectX connectdefault;
+
+  connectdefault.placePiece(4);
+  ASSERT_EQ(1 , connectdefault.whoseTurn());
+  connectdefault.placePiece(5);
+  ASSERT_EQ(2 , connectdefault.whoseTurn());
+  connectdefault.placePiece(5);
+  ASSERT_EQ(1 , connectdefault.whoseTurn());
+  connectdefault.showBoard();
+}
+
+TEST(ConnectXTest, placePieceOutBoard)
+{
+  ConnectX connectdefault;
+
+  connectdefault.placePiece(8);
+  ASSERT_EQ(1 , connectdefault.whoseTurn());
+  connectdefault.placePiece(9);
+  ASSERT_EQ(2 , connectdefault.whoseTurn());
+  connectdefault.placePiece(-2);
+  ASSERT_EQ(1 , connectdefault.whoseTurn());
+
+
+  //the below test fails due to abort() function
+  //being called, which is called by malloc due to
+  //heap overflow problem, this is a bug which needs to be fixed
+
+  //connectdefault.placePiece(-1);
+  //ASSERT_EQ(2 , connectdefault.whoseTurn());
+
+  connectdefault.showBoard();
+}
+
+
+TEST(ConnectXTest, ToggleTest)
+{
+    ConnectX connectparameter(2,2,2);
+    connectparameter.placePiece(1);
+    ASSERT_EQ(1 , connectparameter.whoseTurn());
+    connectparameter.placePiece(1);
+    ASSERT_EQ(2 , connectparameter.whoseTurn());
+    connectparameter.placePiece(1);
+    ASSERT_EQ(1 , connectparameter.whoseTurn());
 }
---
+++ README.md
@@ -0,0 +1,27 @@
+***************************
+Name- Sumeet Tulsani
+***************************
+
+* List of Bugs Found while testing ConnectX Game:
+
+- Out of bound width and in bound height bug:
+  This bug occurs in inbound function when width is greater than the maximum width or less than zero and 
+  the height is in valid range. Firstly, when the width is passed to this function which is invalid it 
+  sets the flag to false, which is correct but then it falls in another if which is used to check the 
+  height passed to the function and if the height is in range it sets the flag back to true which causes
+  a bug in the code. This function also results in failure of another functions like the 'at' function, 
+  'placePiece' function and 'showBoard' function as they call inbound function.
+
+- Piece being placed outside the board bug:
+  This bug occurs in placePiece function when value of column passed to this function is out of range column
+  value. The code in placePiece function calls 'at' function which takes column value passed to the placePiece 
+  function as the first parameter and i(which is less than height-1 and greather than zero) as the second 
+  parameter. Then the 'at' function calls the inbound function by sending the column value as width and i as 
+  height value for inbound function resulting into above described bug, thus the position is returned as valid
+  which is then compared to 'EMPTY' to see if the location is empty on board. This wrong comparision is then 
+  anded with the inbound condition which also returns valid everytime due to the bug present in the inbound
+  function. This results in placing of piece in invalid location, which gives a double free() error, which is
+  error in malloc() function calling abort() function due to heap overflow problem when column value is passed 
+  as "-1". Other invalid locations does not produce double free() error but the code falls in if loop even if
+  the condition is expected to fail.
+  
